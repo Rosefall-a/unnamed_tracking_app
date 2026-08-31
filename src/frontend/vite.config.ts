543ad731c8faba2c27515e06c0f207909f661fe9
Hotzings <hotzings@gmail.com>
2026-08-31 13:51:52 -0400

    Removed useless exposed ports from compose.yml
    fixed up the config.py for backend
    
    swapped internal frontend port to be port 80 to make it more clear how the user should configure it

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [vue()],
   server: {
     host: true, // listen on 0.0.0.0 so the container's port mapping works
+    port: 80,
     proxy: {
       '/api': {
         target: 'http://backend:8000',
