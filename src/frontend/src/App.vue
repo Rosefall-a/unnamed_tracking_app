ba54a69664c1bdba1fb948bba8f0a8059252a956
Olie <olie@obsoletelabs.net>
2026-09-15 16:41:58 +0800

    Fix and complete global route loading indicator

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { computed, ref } from "vue";
 import { useRoute } from "vue-router";
+import router from "./router";
 import SidebarNav from "./components/SidebarNav.vue";
 import DesktopSidebar from "./components/DesktopSidebar.vue";
 import ProfileChip from "./components/ProfileChip.vue";
