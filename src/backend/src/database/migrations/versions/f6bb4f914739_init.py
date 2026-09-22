281e7e4419d1b0fc08c3dd0397a856cd79418f65
Zings <75098398+hotzings@users.noreply.github.com>
2026-09-22 01:26:51 -0400

    Cascade game cleanup in initial schema

--- src/backend/src/database/migrations/versions/f6bb4f914739_init.py
+++ src/backend/src/database/migrations/versions/f6bb4f914739_init.py
@@ -176,6 +176,7 @@ def upgrade() -> None:
         sa.ForeignKeyConstraint(
             ["user_id"],
             ["users.id"],
+            ondelete="CASCADE",
         ),
         sa.PrimaryKeyConstraint("id"),
     )
