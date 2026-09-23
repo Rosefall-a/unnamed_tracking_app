7613469bccd7f187881b1f124accd6e95979da7c
Olie <olie@obsoletelabs.net>
2026-09-23 17:48:17 +0800

    fix finding kind

--- src/backend/src/api/routes/game_archives.py
+++ src/backend/src/api/routes/game_archives.py
@@ -94,7 +94,7 @@ def get_archive_kind_from_string(kind_str: str) -> ArchiveKind:
 def _archive_dir(
     game_folder: str, kind: str, archive_id: UUID, user_id: UUID
 ) -> Path:  # Kind should be ArchiveKind, however its easier to accept type of string and let the caller handle the type checking. This is because the kind is passed in from the route path parameter which is a string.
-    if kind.type is not ArchiveKind:
+    if type(kind) is not ArchiveKind:
         kind = get_archive_kind_from_string(kind)
     return (
         _DATA_ROOT / str(user_id) / "games" / game_folder / _ARCHIVE_SUBDIRS[kind] / str(archive_id)
